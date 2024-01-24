e667cc8cfa0b8e7e049c6d3c7d43ce415207dd92
iomullony <isabel@rugania.es>
2024-01-24 17:35:45 +0100

    First template for login/out

--- WEB/app.py
+++ WEB/app.py
@@ -1,5 +1,9 @@
+import os
+
+from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required
 
@@ -21,6 +25,11 @@ def after_request(response):
     return response
 
 
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -29,36 +38,7 @@ def login():
     # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return apology("must provide username", 403)
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return apology("must provide password", 403)
-
-        # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
-
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
-            return apology("invalid username and/or password", 403)
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
-    # User reached route via GET (as by clicking a link or via redirect)
-    else:
-        return render_template("login.html")
+    return render_template("login.html")
 
 
 @app.route("/logout")
@@ -68,6 +48,12 @@ def logout():
     # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
     return redirect("/")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    return render_template("register.html")
+    
